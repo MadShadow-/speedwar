3aa12e7b4e6417a563071fe992ae4f6b88a404cf
MadShadow- <none>
2019-03-16 19:59:30 +0100

    ReconnectTool v1

--- tools/ReconnectTool.lua
+++ tools/ReconnectTool.lua
@@ -1,67 +1,97 @@
-SW = SW or {}
--- PROBLEM:
--- SPIEL PAUSIERT -> SIMPLE JOB WIRD NICHT AUSGEFÜHRT
--- EINKLINKEN IN IRGENDWAS, WAS PRO FRAME GECALLT WIRD, SEHR SINNVOLL
-SW.Reconnecter = {}
-SW.Reconnecter.Stages = {
-	[1] = function() XNetworkUbiCom.Manager_Destroy() end,
-	[2] = function() XNetworkUbiCom.Manager_Create() end,
-	[3] = function() XNetworkUbiCom.Manager_LogIn_Connect() end,
-	[4] = function() XNetworkUbiCom.Manager_LogIn_Start() end,
-	[5] = function() XNetworkUbiCom.Lobby_Group_Enter(NETWORK_GAME_LOBBY) end,
-	[6] = function() XNetwork.Chat_SendMessageToAll(".join") end,
-	[7] = function() CNetwork.set_ready() end,
-	[8] = function() CNetwork.send_need_ticks(Network_GetLastTickReceived() + 2000) end
-}
-SW.Reconnecter.StageNames = {
-	[1] = "Alte Verbindung wird gekappt.",
-	[2] = "Bereite Siedler vor.",
-	[3] = "Verbinde zu ubi.com.",
-	[4] = "Starte LogIn-Prozess.",
-	[5] = "Trete Raum bei.",
-	[6] = "Trete Spiel bei.",
-	[7] = "Spielstand wird vorbereitet.",
-	[8] = "Fordere neue Ticks an."
-}
-SW.Reconnecter.CurrentStage = 0
---[[
-XNetworkUbiCom.Manager_Destroy();
+SW = SW or {};
+SW.Reconnect = {
+	StartedReconnect = false,
+	LastActionTimestamp = 0,
+	DelayNextAction = 0,
+	CurrentActionIndex = 0,
+	Text = " Verbindung verloren ",
+	Started = false,
+	GameNotOpenAnymore = false,
+	Actions =
+	{
+		[1] = function()
+			CUtil.SetGameTimeFactor(1);
+			XNetworkUbiCom.Manager_Destroy();
+			XNetworkUbiCom.Manager_Create();
+			SW.Reconnect.Started = true;
+			SW.Reconnect.Text = " Verbinden ";
+			XGUIEng.ShowWidget("SWReconnectState",1);
+			return 1;
+		end,
+		
+		[2] = XNetworkUbiCom.Manager_LogIn_Connect,
+		[3] = XNetworkUbiCom.Manager_LogIn_Start,
+		
+		[4] = function()
+			return XNetworkUbiCom.Lobby_Group_Enter(NETWORK_GAME_LOBBY);
+		end,
+		
+		[5] = function()
+			XNetworkUbiCom.Lobby_Group_Enter(NETWORK_GAME_LOBBY);
+			--if XNetworkUbiCom.Lobby_Group_GetIndexOfCurrent() == -1 then
+				--SW.Reconnect.GameNotOpenAnymore = true;
+				--return 1;
+			--end
+			XNetwork.Chat_SendMessageToAll(".join"); return 1;
+		end,
+		
+		[6] = function()
+			CNetwork.set_ready();
+			return 1;
+		end,
+		
+		[7] = function()
+			CNetwork.send_need_ticks(Network_GetLastTickReceived() + 2000);
+			-- reset params
+			SW.Reconnect.Started = false;
+			SW.Reconnect.CurrentActionIndex = 0;
+			-- update GUI
+			SW.Reconnect.Text = " @color:0,255,0 Verbunden! ";
+			local count = 3;
+			SW_Reconnect_Hide = function()
+				if count > 0 then count = count - 1; return; end
+				XGUIEng.ShowWidget("SWReconnectState",0);
+				return true;
+			end
+			StartSimpleJob("SW_Reconnect_Hide");
+			XGUIEng.SetText( "SWReconnectState", SW.Reconnect.Text);
+			XNetwork.Manager_Create();
+			return 1;
+		end,
+	},
+};
 
-XNetworkUbiCom.Manager_Create()
-XNetworkUbiCom.Manager_LogIn_Connect()
-XNetworkUbiCom.Manager_LogIn_Start()
-XNetworkUbiCom.Lobby_Group_Enter(NETWORK_GAME_LOBBY)
-XNetwork.Chat_SendMessageToAll(".join");
-CNetwork.set_ready();
-CNetwork.send_need_ticks(Network_GetLastTickReceived() + 2000);
-]]
-function SW.Reconnecter.Init()
-	if CNetwork == nil then return end
-	SW.Reconnecter.GameCallback_GUI_ChatStringInputDone = GameCallback_GUI_ChatStringInputDone
-	GameCallback_GUI_ChatStringInputDone = function(_msg, _wId)
-		if _msg == ".reconnect" then
-			SW.Reconnecter.StartReconnectProcess()
-		else
-			SW.Reconnecter.GameCallback_GUI_ChatStringInputDone(_msg, _wId)
+function SW.Reconnect.UpdateEveryFrame()
+	if not SW.Reconnect.Started then
+		if table.getn(GetIngamePlayers()) > 0 then
+			return;
+		end
+		if table.getn(GetSpectators()) > 0 then
+			return;
 		end
 	end
-end
-function SW.Reconnecter.StartReconnectProcess()
-	if SW.Reconnecter.Running then Message("Reconnecter is already running!") return end
-	SW.Reconnecter.Running = true
-	SW.Reconnecter.CurrentStage = 0
-	StartSimpleJob("SW_Reconnecter_Job")
-end
-function SW_Reconnecter_Job()
-	if Counter.Tick2("ReconnectorJob", 5) then
-		SW.Reconnecter.CurrentStage = SW.Reconnecter.CurrentStage + 1
-		if SW.Reconnecter.Stages[SW.Reconnecter.CurrentStage] ~= nil then
-			Message("Stage "..SW.Reconnecter.CurrentStage..": "..SW.Reconnecter.StageNames[SW.Reconnecter.CurrentStage])
-			SW.Reconnecter.Stages[SW.Reconnecter.CurrentStage]()
-		else
-			SW.Reconnecter.Running = false
-			Message("Reconnect abgeschlossen.")
-			return true
+	if SW.Reconnect.GameNotOpenAnymore then
+		XGUIEng.SetText( "SWReconnectState", " @color:255,0,0 Spiel nicht mehr offen! Kann nicht verbinden! ");
+		return;
+	end
+	-- disconnected => reconnect
+	local currentTimestamp = math.floor(XGUIEng.GetSystemTime());
+	local delay = currentTimestamp - SW.Reconnect.LastActionTimestamp;
+	XGUIEng.SetText( "SWReconnectState", "("..SW.Reconnect.CurrentActionIndex.."/7) "..SW.Reconnect.Text.." in "..(SW.Reconnect.DelayNextAction-delay));
+	if delay > SW.Reconnect.DelayNextAction then
+		-- update timestamp
+		SW.Reconnect.LastActionTimestamp = currentTimestamp;
+		-- next action
+		SW.Reconnect.CurrentActionIndex = SW.Reconnect.CurrentActionIndex + 1;
+		-- execute
+		local res = SW.Reconnect.Actions[SW.Reconnect.CurrentActionIndex]();
+		if res ~= 1 then
+			-- action failed
+			SW.Reconnect.CurrentActionIndex = 0;
+			SW.Reconnect.DelayNextAction = 5;
+			SW.Reconnect.Text = " @color:255,0,0 Fehlgeschlagen! ";
+			return;
 		end
+		SW.Reconnect.DelayNextAction = 2;
 	end
 end
